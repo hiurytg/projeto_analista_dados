2104743776cd040439831091dc4fb39387a66655
hiurytg <77080618+hiurytg@users.noreply.github.com>
2025-07-06 20:36:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -26,27 +26,28 @@ def login():
             st.error("Invalid username or password")
 
 # Main app logic
-if "logged_in" not in st.session_state:
+while "logged_in" not in st.session_state:
     login()
-else:
-    source = "licitacoes.csv"
-    conn   = sqlite.connect('licitacoes.db')
-    df     = pd.read_csv(source, delimiter =';', on_bad_lines = 'skip')
-    query  = "SELECT * FROM tblicitacoes limit 20" 
+    break
     
-    df.to_sql('tblicitacoes', conn, if_exists='replace') 
-    
-    result_df = pd.read_sql_query(query, conn)
-    
-    
-    # Streamlit
-    st.title("Dados de licitações no Brasil")
-    st.write("Tabela oficial .gov:")
-    st.dataframe(result_df)
-    
-    # Add more interactive elements as needed
-    if st.button('Show Summary'):
-        st.write(result_df.describe())
-    
-    conn.close()
-    
+source = "licitacoes.csv"
+conn   = sqlite.connect('licitacoes.db')
+df     = pd.read_csv(source, delimiter =';', on_bad_lines = 'skip')
+query  = "SELECT * FROM tblicitacoes limit 20" 
+
+df.to_sql('tblicitacoes', conn, if_exists='replace') 
+
+result_df = pd.read_sql_query(query, conn)
+
+
+# Streamlit
+st.title("Dados de licitações no Brasil")
+st.write("Tabela oficial .gov:")
+st.dataframe(result_df)
+
+# Add more interactive elements as needed
+if st.button('Show Summary'):
+    st.write(result_df.describe())
+
+conn.close()
+
